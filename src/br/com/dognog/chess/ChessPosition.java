d4bbd12539f16f68855286af639cf83342c9f6aa
Douglas Nogueira <douglaswnogueira@hotmail.com>
2023-10-18 23:13:40 -0300

    improve impression

--- src/br/com/dognog/chess/ChessPosition.java
+++ src/br/com/dognog/chess/ChessPosition.java
@@ -7,10 +7,13 @@ public class ChessPosition {
     private char column;
     private int row;
 
+
     public ChessPosition(char column, int row) {
         if(column < 'a' || column > 'h' || row < 1 || row > 8) {
             throw new ChessException("Error instantiating ChessPosition. Valid values are between a1 and h8");
         }
+        this.column = column;
+        this.row = row;
     }
 
     public char getColumn() {
