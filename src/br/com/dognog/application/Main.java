1b5834a7d2301bc7c339666bf0e0db6bf4b82f8b
Douglas Nogueira <douglaswnogueira@hotmail.com>
2023-10-20 08:09:13 -0300

    printing possible moves

--- src/br/com/dognog/application/Main.java
+++ src/br/com/dognog/application/Main.java
@@ -22,6 +22,10 @@ public class Main {
                 System.out.println("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMacth.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMacth.getPieces(), possibleMoves);
+
                 System.out.println();
                 System.out.println("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
